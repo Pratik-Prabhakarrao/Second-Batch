e4d8afa81bc7920f3a565eab084566738b01b64c
Pratik Prabhakarrao <Pratikprabhakarraoatwork@gmail.com>
2025-04-01 23:47:32 +0530

    first commit

---
+++ first.js
@@ -0,0 +1,140 @@
+// // console.log(25 + 40);
+// // console.log("hello I live in gwalior");
+// // let number = Math.trunc(Math.random() * 10);
+// // console.log(number);
+
+// console.log(4 + 6);
+
+// let number = 10;
+// let y = 20;
+// let z = 6; //not descriptive enough
+
+// //convention  --- can break it
+// let firstName = "Sachin";
+
+// let presidentOfIndia = "Droupadi murmu";
+
+// let firstname = "Sachin"; // small case
+// let first_Name = "Sachin";
+// let FirstName = "Sachin"; // pascal case or upper camal case
+// let Firstname = "Sachin";
+// let FIRSTNAME = "Sachin"; // constant
+// let lastName = "Tendulkar"; // camal case
+
+// let PI = 3.14;
+// let MAXLIMIT = 50;
+// let AVAGADRONUMBER =
+//   // let prime_minister_of_india ="Narendra Modi"   /// snake case
+
+// // console.log(x + y);
+// // console.log(x + z);
+
+// // let 99acers = "website for property";
+
+let $price = 59;
+console.log($price);
+
+let _name = "rahul";
+
+// let mahindra&mahindra = "tractor" // invalid
+
+let presidentOfIndia = "Droupadi murmu"; // assignment operator
+
+console.log(presidentOfIndia);
+
+let isShopOpen = true;
+
+console.log(isShopOpen);
+
+// How to declare variable
+
+// let --- temporary variable or subject value change  , const , var ,
+
+// let const and var are to be used only once while decleration
+
+const numberOfCenturyOfSachin = 100; // assingment of value to variable
+// numberOfCenturyOfSachin = 101; // reassignment of value to existing variable
+//variable declered with const can't be reassigned
+
+let centuriesOfVirat = 83;
+centuriesOfVirat = 84;
+
+console.log(numberOfCenturyOfSachin);
+console.log(centuriesOfVirat);
+
+//var
+
+var heightOfMangoTree = 12;
+
+heightOfMangoTree = 14;
+console.log(heightOfMangoTree);
+
+// console.log(window);
+
+weight = 140; // if we do not declare variable with let, const , var then it is by default var
+
+console.log(weight);
+
+// console.log(window);
+
+let streetName;
+const cityName = "Gwalior";
+var countryName;
+
+console.log(typeof streetName);
+
+// cityName = "gwalior";
+
+streetName = "MORAR road";
+console.log(typeof streetName);
+
+console.log(typeof centuriesOfVirat);
+
+console.log(typeof isShopOpen);
+
+isLightOn = false;
+console.log(typeof isLightOn);
+
+console.log(centuriesOfVirat);
+
+// string decleration rule
+
+countryName = "India";
+const middleName = "Ramesh";
+const village = `Tedule`;
+
+let pappu = `pappu cant't daNCE SALA"PAPPU CAN'T Dance sala... "Mkg" `;
+
+const emptyValue = null; // intentional absence of value
+console.log(emptyValue); //  value - null
+console.log(typeof emptyValue); // type - object
+console.log(typeof null); // type - object
+
+let cost;
+
+console.log(cost); // value - undefined
+
+console.log(typeof cost); //type - undefined
+
+console.log(Number.MAX_SAFE_INTEGER);
+
+// BigInt
+
+const num1 = 23n;
+console.log(typeof num1); // Bigint
+
+const num2 = 2n; // if no n is applied we can't multiply
+//only operationm of bigInt is possible
+
+console.log(num1 * num2);
+
+//coment and multiline comment
+
+// first type
+//  ctrl + /
+/* Gwalior is clean city 
+and this is not a joke  */
+
+console.log(typeof 23);
+console.log("hello");
+console.log("hello");
